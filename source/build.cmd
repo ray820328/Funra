5386b04a0bb358e307b4de5823490d90a6605832
yirui2 <yirui2@kingsoft.com>
2022-06-06 11:01:40 +0800

    project init

--- source/build.cmd
+++ source/build.cmd
@@ -6,12 +6,13 @@ if "%1"=="3rd" (
     echo "build 3rd..."
 ) else (
     echo  "start building funra..."
-    SET FUNRAY_BUILD_OPTIONS=-DCMAKE_BUILD_TYPE="Release"
+    SET FUNRAY_BUILD_OPTIONS=-DCMAKE_C_FLAGS="-g -wall"
 	SET "SRC_RBASE_ROOT=%ROOT_PATH%/source/rbase"
 	SET "DEST_RBASE_ROOT=%ROOT_PATH%/proj/vs2017/rbase"
 	::MD "!DEST_RBASE_ROOT!/build"
     cmake -S "!SRC_RBASE_ROOT!" -B "!DEST_RBASE_ROOT!" !FUNRAY_BUILD_OPTIONS! -A x64
     ::cmake --build "!DEST_RBASE_ROOT!" --config Debug --target install
+	cmake --build "!DEST_RBASE_ROOT!" --config Debug
     cmake --build "!DEST_RBASE_ROOT!" --config Release
 
     ::MOVE /Y "%INSTALL_PREFIX%/rcommon.lib" "%INSTALL_PREFIX%"
