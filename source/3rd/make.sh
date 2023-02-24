4c0a460d629acc0b857119b055438117eec85075
yirui2 <yirui2@kingsoft.com>
2023-02-24 19:43:36 +0800

    编译脚本同步

--- source/3rd/make.sh
+++ source/3rd/make.sh
@@ -10,14 +10,17 @@ mkdir -p build/Release
 #cmake -S ${ROOT_PATH} -B ${ROOT_PATH}/build/Debug -DCMAKE_BUILD_TYPE=Debug ${LUA_BS_BUILD_OPTIONS} && cmake --build ${ROOT_PATH}/build/debug --target install -j ${CPU_CORE_NUM}
 #cmake -S ${ROOT_PATH} -B ${ROOT_PATH}/build/Release -DCMAKE_BUILD_TYPE=Release ${LUA_BS_BUILD_OPTIONS} && cmake --build ${ROOT_PATH}/build/release --target install -j ${CPU_CORE_NUM}
 
-cd build/Debug
-cmake -DCMAKE_BUILD_TYPE=Debug ../..
-make -j $coreCount $@
+if [[ ${BUILD_PACKAGE_TYPE} = "All" || ${BUILD_PACKAGE_TYPE} = "Debug" ]];then
+    cd build/Debug
+    cmake -DCMAKE_BUILD_TYPE=Debug ../..
+    make -j $coreCount $@
+fi
 
-
-cd ../Release
-cmake -DCMAKE_BUILD_TYPE=Release ../..
-make -j $coreCount $@
+if [[ ${BUILD_PACKAGE_TYPE} = "All" || ${BUILD_PACKAGE_TYPE} = "Release" ]];then
+    cd ../Release
+    cmake -DCMAKE_BUILD_TYPE=Release ../..
+    make -j $coreCount $@
+fi
 
 cd ${ROOT_PATH}
 rm -rf build
